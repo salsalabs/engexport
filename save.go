6072686bc1c9b716206761e1c98b04d252de975c
Allen Leonard <aleonard@salsalabs.com>
2018-11-06 14:13:52 -0600

    Force add_tags version.

--- save.go
+++ save.go
@@ -34,50 +34,7 @@ func (env *E) Save() {
 		var a []string
 		for _, k := range env.Headers {
 			m := env.Fields[k]
-<<<<<<< HEAD
-			//KLUDGE:  Salsa wants to see supporter.supporter_KEY/supporter.Email
-			// in the conditions and included fields.  However, the data is stored
-			// simply as "supporter_KEY" or "Email"...
-			i := strings.Index(m, ".")
-			if i != -1 {
-				m = strings.Split(m, ".")[1]
-			}
-
-			s, ok := d[m]
-			if !ok {
-				s = ""
-			}
-			//Transform fields as needed.  This includes making pretty dates,
-			//setting the Engage transaction type and putting Engage text into
-			//Receive_Email.
-			switch m {
-			case "friend_of_a_friend___first_name":
-				//merge this and friend_of_a_friend___last_name
-				//into friend_of_a_friend_name
-			case "State":
-				s = strings.ToUpper(s)
-			case "Transaction_Date":
-				s = godig.EngageDate(s)
-			case "Transaction_Type":
-				if s != "Recurring" {
-					s = "OneTime"
-				}
-			case "Receive_Email":
-				t := "Unsubscribed"
-				x, err := strconv.ParseInt(s, 10, 32)
-				if err == nil && x > 0 {
-					t = "Subscribed"
-				}
-				s = t
-			}
-
-			// Convert tabs to spaces. Remove leading/trailing spaces.
-			// Append the cleaned-up value to the output.
-			s = strings.Replace(s, "\t", " ", -1)
-			s = strings.TrimSpace(s)
-=======
 			s := transform(m, d)
->>>>>>> add_tags
 			a = append(a, s)
 		}
 		err := w.Write(a)
@@ -94,8 +51,6 @@ func (env *E) Save() {
 		f.Close()
 	}
 }
-<<<<<<< HEAD
-=======
 
 //transform cleans up the value and returns it.
 func transform(m string, d R) string {
@@ -139,4 +94,3 @@ func transform(m string, d R) string {
 
 	return s
 }
->>>>>>> add_tags
